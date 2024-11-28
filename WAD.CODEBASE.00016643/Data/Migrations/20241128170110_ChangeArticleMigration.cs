a137badd59fe673c9127d21c9ca1a903fbe1fe8c
00016643 <00016643wiut@gmail.com>
2024-11-28 22:02:39 +0500

    Deleted unnecessary files

---
+++ WAD.CODEBASE.00016643/Data/Migrations/20241128170110_ChangeArticleMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WAD.CODEBASE._00016643.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeArticleMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
